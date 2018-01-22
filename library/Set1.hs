5d3df4e0ad6ce4677aaa7bb7acb75ef5441b3b92
ugly_bear <goosetherumfoodle@users.noreply.github.com>
2018-01-22 01:52:35 -0500

    red test for challenge 2

--- library/Set1.hs
+++ library/Set1.hs
@@ -2,13 +2,13 @@
 
 module Set1 where
 
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Base64 as BS64
+import           Data.ByteString        (ByteString)
+import qualified Data.ByteString        as BS
 import qualified Data.ByteString.Base16 as BS16
+import qualified Data.ByteString.Base64 as BS64
 
 newtype ErrorString a = ErrorString {runError :: a} deriving (Show, Eq)
-newtype HexString a = HexString {runHex :: a} deriving Show
+newtype HexString a = HexString {runHex :: a} deriving (Show, Eq)
 newtype Base64String a = Base64String {runBase64 :: a} deriving (Show, Eq)
 
 -- Challenge 1
@@ -28,10 +28,32 @@ newtype Base64String a = Base64String {runBase64 :: a} deriving (Show, Eq)
 
 -- Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
 
-
 hexToBase64 :: HexString ByteString -> Either (ErrorString ByteString) (Base64String ByteString)
 hexToBase64 a = Base64String . BS64.encode <$> (decodeHex a)
 
 decodeHex :: HexString ByteString -> Either (ErrorString ByteString) ByteString
 decodeHex a | (decoded, "") <- BS16.decode . runHex $ a = Right decoded
             | otherwise = Left . ErrorString $ BS.concat ["invalid hex: ", runHex a]
+
+-- Challenge 2
+-- Fixed XOR
+
+-- Write a function that takes two equal-length buffers and produces their XOR combination.
+
+-- If your function works properly, then when you feed it the string:
+
+-- 1c0111001f010100061a024b53535009181c
+
+-- ... after hex decoding, and when XOR'd against:
+
+-- 686974207468652062756c6c277320657965
+
+-- ... should produce:
+
+-- 746865206b696420646f6e277420706c6179
+
+bytesToHex :: ByteString -> HexString ByteString
+bytesToHex = undefined
+
+xor :: ByteString -> ByteString -> ByteString
+xor = undefined
--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -3,7 +3,7 @@
 import qualified Test.Tasty
 import Test.Tasty.Hspec
 import Set1
-
+import Data.ByteString
 
 main :: IO ()
 main = do
@@ -13,8 +13,13 @@ main = do
 spec :: Spec
 spec = parallel $ do
   describe "Set1" $ do
-    describe "hexToBase64" $ do
+    describe "Challenge 1" $ do
       it "translates hex string to correct base64 string" $ do
         let expected = Right . Base64String $ "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
             result = hexToBase64 $ HexString "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
         result `shouldBe` expected
+    describe "Challenge 2" $ do
+      it "correctly XORs hex strings" $ do
+        let firstBytes = decodeHex . HexString $ "1c0111001f010100061a024b53535009181c"
+            secondBytes = decodeHex . HexString $ "686974207468652062756c6c277320657965"
+        (bytesToHex . xor <$> firstBytes <*> secondBytes) `shouldBe` (Right . HexString $ ("746865206b696420646f6e277420706c6179" :: ByteString))
